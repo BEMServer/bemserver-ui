54dc5410ce62a22fac37863320be78ef52cd53b2
David FREDERIQUE <dfrederique@nobatek.inef4.com>
2022-03-23 16:44:54 +0100

    Update users list feature

--- bemserver_ui/extensions/error_handlers.py
+++ bemserver_ui/extensions/error_handlers.py
@@ -5,6 +5,13 @@ import werkzeug.exceptions as wexc
 
 def init_app(app):
 
+    def _get_back_location(endpoint="main.index"):
+        return (
+            flask.request.args.get("back")
+            or flask.request.referrer
+            or flask.url_for(endpoint)
+        )
+
     @app.errorhandler(wexc.Unauthorized)
     def handle_401(_):
         flask.flash("Incorrect or missing credentials", "error")
@@ -14,3 +21,9 @@ def init_app(app):
     def handle_403(_):
         flask.flash("Insufficient permissions", "error")
         return flask.redirect(flask.url_for("main.index"))
+
+    @app.errorhandler(wexc.UnprocessableEntity)
+    def handle_422(exc):
+        flask.flash(exc.description or "Operation failed!", "error")
+        flask.session["_validation_errors"] = exc.response or {}
+        return flask.redirect(_get_back_location())
--- bemserver_ui/views/users.py
+++ bemserver_ui/views/users.py
@@ -2,22 +2,19 @@
 import flask
 
 import bemserver_ui.extensions.api_client as bac
-from bemserver_ui.extensions import auth
+from bemserver_ui.extensions import auth, Roles
 
 
 blp = flask.Blueprint("users", __name__, url_prefix="/users")
 
 
 @blp.route("/")
-@auth.signin_required
+@auth.signin_required(roles=[Roles.admin])
 def list():
     # TODO: add filters
     try:
         users_resp = flask.g.api_client.users.getall()
     except bac.BEMServerAPIValidationError as exc:
-        flask.session["_validation_errors"] = exc.errors
-        flask.flash("Operation failed!", "error")
-        return flask.redirect(flask.request.referrer)
+        flask.abort(422, description=exc.errors)
 
-    # TODO: manage pagination, if any
     return flask.render_template("pages/users/list.html", users=users_resp.data)
