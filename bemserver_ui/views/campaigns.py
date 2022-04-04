cb3b2e1e2d17ef32f3c648efd9f20734ca6da470
David FREDERIQUE <dfrederique@nobatek.inef4.com>
2022-04-04 17:41:19 +0200

    Add manage campaign's user groups feature

--- bemserver_ui/views/campaigns.py
+++ bemserver_ui/views/campaigns.py
@@ -39,8 +39,23 @@ def view(id):
     except bac.BEMServerAPINotFoundError:
         flask.abort(404, description="Campaign not found!")
 
+    # Get campaign's user groups.
+    ugroups_resp = flask.g.api_client.user_groups_by_campaigns.getall(campaign_id=id)
+    ugroups = []
+    for x in ugroups_resp.data:
+        try:
+            ugroup_resp = flask.g.api_client.user_groups.getone(id=x["user_group_id"])
+        except bac.BEMServerAPINotFoundError:
+            # Here, just ignore if a user group has been deleted.
+            pass
+        else:
+            ugroup_data = ugroup_resp.data
+            ugroup_data["rel_id"] = x["id"]
+            ugroups.append(ugroup_data)
+
     return flask.render_template(
-        "pages/campaigns/view.html", campaign=campaign.data, etag=campaign.etag)
+        "pages/campaigns/view.html", campaign=campaign.data, etag=campaign.etag,
+        user_groups=ugroups)
 
 
 @blp.route("/create", methods=["GET", "POST"])
@@ -146,3 +161,65 @@ def delete(id):
         flask.flash("Campaign deleted!", "success")
 
     return flask.redirect(flask.url_for("campaigns.list"))
+
+
+@blp.route("/<int:id>/manage_groups", methods=["GET", "POST"])
+@auth.signin_required(roles=[Roles.admin])
+def manage_groups(id):
+    try:
+        campaign = flask.g.api_client.campaigns.getone(id)
+    except bac.BEMServerAPINotFoundError:
+        flask.abort(404, description="Campaign not found!")
+
+    if flask.request.method == "POST":
+        user_group_ids = [x.split("-")[1] for x in flask.request.form.keys()]
+        for user_group_id in user_group_ids:
+            try:
+                flask.g.api_client.user_groups_by_campaigns.create({
+                    "campaign_id": id,
+                    "user_group_id": user_group_id,
+                })
+            except bac.BEMServerAPIValidationError as exc:
+                flask.abort(
+                    409, description=(
+                        "An error occured while trying to add the user in a group!"),
+                    response=exc.errors)
+        if len(user_group_ids) > 0:
+            flask.flash("User added to selected group(s)!", "success")
+
+    # Get campaign's user groups.
+    groups_resp = flask.g.api_client.user_groups_by_campaigns.getall(campaign_id=id)
+    groups = []
+    group_ids = []
+    for x in groups_resp.data:
+        group_resp = flask.g.api_client.user_groups.getone(id=x["user_group_id"])
+        group_data = group_resp.data
+        group_data["rel_id"] = x["id"]
+        groups.append(group_data)
+        group_ids.append(group_data["id"])
+
+    # Get available groups (all groups - campaign's user groups).
+    all_groups_resp = flask.g.api_client.user_groups.getall()
+    available_groups = []
+    for x in all_groups_resp.data:
+        if x["id"] not in group_ids:
+            available_groups.append(x)
+
+    return flask.render_template(
+        "pages/campaigns/manage_groups.html", campaign=campaign.data,
+        etag=campaign.etag, user_groups=groups, available_groups=available_groups)
+
+
+@blp.route("/<int:id>/remove_user_group", methods=["POST"])
+@auth.signin_required(roles=[Roles.admin])
+def remove_user_group(id):
+    user_groups_by_campaign_id = flask.request.args["rel_id"]
+    try:
+        flask.g.api_client.user_groups_by_campaigns.delete(user_groups_by_campaign_id)
+    except bac.BEMServerAPINotFoundError:
+        flask.abort(
+            404, description="User group has already been removed from this campaign!")
+    else:
+        flask.flash("User group removed from campaign!", "success")
+
+    return flask.redirect(flask.request.args["next"])
