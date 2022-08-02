94691d1cc0c212006cf032e043e117dc78b233ac
Aymen Tilfani <atilfani@nobatek.inef4.com>
2022-08-02 10:42:58 +0200

    Update API client completeness

--- bemserver_ui/extensions/api_client/resources.py
+++ bemserver_ui/extensions/api_client/resources.py
@@ -367,28 +367,29 @@ class IOResources(BaseResources):
 
 
 class AnalysisResources(BaseResources):
-    endpoint_base_uri = "/analysis/campaign/"
+    endpoint_base_uri = "/analysis/"
     disabled_endpoints = ["getall", "getone", "create", "update", "delete"]
 
     def get_completeness(
         self,
-        campaign_id,
         start_time,
         end_time,
         timeseries,
         data_state,
-        bucket_width,
+        bucket_width_value,
+        bucket_width_unit,
         timezone="UTC",
         *,
         etag=None,
     ):
-        endpoint = f"{self.endpoint_base_uri}{campaign_id}/completeness"
+        endpoint = f"{self.endpoint_base_uri}completeness"
         q_params = {
             "start_time": start_time,
             "end_time": end_time,
             "timeseries": timeseries,
             "data_state": data_state,
-            "bucket_width": bucket_width,
+            "bucket_width_value": bucket_width_value,
+            "bucket_width_unit": bucket_width_unit,
             "timezone": timezone,
         }
         return self._req.getall(endpoint, etag=etag, params=q_params)
