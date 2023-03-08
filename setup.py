63a8738b874add3ea0dcf4500fd574ba645f4545
David FREDERIQUE <dfrederique@nobatek.inef4.com>
2023-03-08 10:44:45 +0100

    Support Python 3.11

--- setup.py
+++ setup.py
@@ -36,9 +36,9 @@ setup(
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         (
             "License :: OSI Approved :: "
             "GNU Affero General Public License v3 or later (AGPLv3+)"
