86ed9bbd1f7edd8f39d12408a70f9a1bb86f578c
David FREDERIQUE <dfrederique@nobatek.inef4.com>
2023-03-23 11:47:03 +0100

    Fix and improve tests on common time tools

--- tests/test_common_time.py
+++ tests/test_common_time.py
@@ -16,6 +16,16 @@ class TestCommonTime:
         assert convert_html_form_datetime("2023-03-22", "10:07", tz=tz) == (
             dt.datetime(2023, 3, 22, 10, 7, tzinfo=tz)
         )
+        assert convert_html_form_datetime("2023-04-22", "10:07", tz=tz) == (
+            dt.datetime(2023, 4, 22, 10, 7, tzinfo=tz)
+        )
+        tz = ZoneInfo("Etc/GMT+1")
+        assert convert_html_form_datetime("2023-03-22", "10:07", tz=tz) == (
+            dt.datetime(2023, 3, 22, 10, 7, tzinfo=tz)
+        )
+        assert convert_html_form_datetime("2023-04-22", "10:07", tz=tz) == (
+            dt.datetime(2023, 4, 22, 10, 7, tzinfo=tz)
+        )
         assert convert_html_form_datetime("2023-03-22", "10:07", tz=None) == (
             dt.datetime(2023, 3, 22, 10, 7)
         )
@@ -33,21 +43,40 @@ class TestCommonTime:
         assert convert_from_iso("2023-03-22T10:07:00+00:00") == (
             dt.datetime(2023, 3, 22, 10, 7, tzinfo=dt.timezone.utc)
         )
-        assert convert_from_iso("2023-03-22T10:07:00") == (
+        assert convert_from_iso("2023-03-22T10:07:00+01:00") == (
             dt.datetime(2023, 3, 22, 9, 7, tzinfo=dt.timezone.utc)
         )
         assert convert_from_iso("2023-03-22T10:07:00+00:00", tz=None) == (
             dt.datetime(2023, 3, 22, 10, 7, tzinfo=dt.timezone.utc)
         )
-        assert convert_from_iso("2023-03-22T10:07:00", tz=None) == (
-            dt.datetime(2023, 3, 22, 10, 7)
+        assert convert_from_iso("2023-03-22T10:07:00+01:00", tz=None) == (
+            dt.datetime(2023, 3, 22, 10, 7, tzinfo=dt.timezone(dt.timedelta(hours=1.0)))
         )
         tz = ZoneInfo("Europe/Paris")
         assert convert_from_iso("2023-03-22T10:07:00+00:00", tz=tz) == (
             dt.datetime(2023, 3, 22, 11, 7, tzinfo=tz)
         )
-        assert convert_from_iso("2023-03-22T10:07:00", tz=tz) == (
+        assert convert_from_iso("2023-03-22T10:07:00+01:00", tz=tz) == (
             dt.datetime(2023, 3, 22, 10, 7, tzinfo=tz)
+        )
+        assert convert_from_iso("2023-04-22T10:07:00+00:00", tz=tz) == (
+            dt.datetime(2023, 4, 22, 12, 7, tzinfo=tz)
+        )
+        assert convert_from_iso("2023-04-22T10:07:00+01:00", tz=tz) == (
+            dt.datetime(2023, 4, 22, 11, 7, tzinfo=tz)
+        )
+        tz = ZoneInfo("Etc/GMT+1")
+        assert convert_from_iso("2023-03-22T10:07:00+00:00", tz=tz) == (
+            dt.datetime(2023, 3, 22, 9, 7, tzinfo=tz)
+        )
+        assert convert_from_iso("2023-03-22T10:07:00+01:00", tz=tz) == (
+            dt.datetime(2023, 3, 22, 8, 7, tzinfo=tz)
+        )
+        assert convert_from_iso("2023-04-22T10:07:00+00:00", tz=tz) == (
+            dt.datetime(2023, 4, 22, 9, 7, tzinfo=tz)
+        )
+        assert convert_from_iso("2023-04-22T10:07:00+01:00", tz=tz) == (
+            dt.datetime(2023, 4, 22, 8, 7, tzinfo=tz)
         )
 
         with pytest.raises(BEMServerUICommonInvalidDatetimeError):
