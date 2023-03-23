43a7aeb9bb0f5fc46c69793fe0422325ce5a7dbd
David FREDERIQUE <dfrederique@nobatek.inef4.com>
2023-03-23 10:42:19 +0100

    Add tests on common tools

---
+++ tests/test_common_tools.py
@@ -0,0 +1,15 @@
+"""Common tools tests"""
+import io
+
+from bemserver_ui.common.tools import is_filestream_empty
+
+
+class TestCommonTools:
+    def test_is_filestream_empty(self):
+        assert is_filestream_empty(None)
+
+        empty_stream = io.BytesIO()
+        assert is_filestream_empty(empty_stream)
+
+        not_empty_stream = io.BytesIO(b"something")
+        assert not is_filestream_empty(not_empty_stream)
