3ab7091ca26b202fad73c09ab3da59f9ae5c6938
David FREDERIQUE <dfrederique@nobatek.inef4.com>
2023-03-28 17:46:29 +0200

    Add tests on common const values

---
+++ tests/test_common_const.py
@@ -0,0 +1,28 @@
+"""Common const tests"""
+from bemserver_ui.common.const import (
+    STRUCTURAL_ELEMENT_TYPES,
+    FULL_STRUCTURAL_ELEMENT_TYPES,
+    BEMSERVER_APP_LABELS,
+    SIDEBAR_SECTIONS,
+)
+
+
+class TestCommonConst:
+    def test_structural_element_types(self):
+        assert STRUCTURAL_ELEMENT_TYPES == ["site", "building", "storey", "space"]
+        assert FULL_STRUCTURAL_ELEMENT_TYPES == [
+            "site",
+            "building",
+            "storey",
+            "space",
+            "zone",
+        ]
+
+    def test_app_labels(self):
+        assert BEMSERVER_APP_LABELS == {
+            "bemserver_core": "Core",
+            "bemserver_api": "API",
+        }
+
+    def test_sidebar_sections(self):
+        assert SIDEBAR_SECTIONS == ["tsdata", "analysis", "dashboards", "services"]
