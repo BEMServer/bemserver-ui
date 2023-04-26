1511a34179e2ff3580e30cad0422b57be92688b7
David FREDERIQUE <dfrederique@nobatek.inef4.com>
2023-04-26 17:43:31 +0200

    Add degree days analysis internal API

--- bemserver_ui/internal_api/analysis/__init__.py
+++ bemserver_ui/internal_api/analysis/__init__.py
@@ -3,6 +3,7 @@ import flask
 
 from . import completeness  # noqa
 from . import energy_consumption  # noqa
+from . import degree_days  # noqa
 
 
 blp = flask.Blueprint("analysis", __name__, url_prefix="/analysis")
@@ -11,4 +12,5 @@ blp = flask.Blueprint("analysis", __name__, url_prefix="/analysis")
 def register_blueprint(api_blp):
     blp.register_blueprint(completeness.blp)
     blp.register_blueprint(energy_consumption.blp)
+    blp.register_blueprint(degree_days.blp)
     api_blp.register_blueprint(blp)
