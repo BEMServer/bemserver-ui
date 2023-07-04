985a523982b5047d3f65d817ddcc8f8ae0a0aac9
David FREDERIQUE <dfrederique@nobatek.inef4.com>
2023-07-04 14:09:46 +0200

    Upgrade bemserver-api-client (>=0.20.1, <0.21.0)

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     install_requires=[
         "flask>=2.2.3,<3.0.0",
         "python-dotenv>=1.0.0,<2.0.0",
-        "bemserver-api-client>=0.20.0,<0.21.0",
+        "bemserver-api-client>=0.20.1,<0.21.0",
     ],
     extras_require=EXTRAS_REQUIRE,
     packages=find_packages(exclude=["tests*"]),
