e9bd1628c11fe65a99f9cf892e9d2443d8bfa177
David FREDERIQUE <dfrederique@nobatek.com>
2025-11-24 11:56:31 +0100

    Upgrade luxon (-> 3.7.2)

--- src/bemserver_ui/static/scripts/modules/tools/time.js
+++ src/bemserver_ui/static/scripts/modules/tools/time.js
@@ -1,3 +1,3 @@
 // Republish luxon modules.
-import { DateTime, Duration, FixedOffsetZone, IANAZone, Info, Interval, InvalidZone, Settings, SystemZone, VERSION, Zone } from "https://cdn.jsdelivr.net/npm/luxon@3.6.1/+esm";
+import { DateTime, Duration, FixedOffsetZone, IANAZone, Info, Interval, InvalidZone, Settings, SystemZone, VERSION, Zone } from "https://cdn.jsdelivr.net/npm/luxon@3.7.2/+esm";
 export { DateTime, Duration, FixedOffsetZone, IANAZone, Info as TimeInfo, Interval, InvalidZone, Settings as TimeSettings, SystemZone, VERSION as TimeVersion, Zone };
